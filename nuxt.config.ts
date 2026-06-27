3ff506ed192f9c6d0e796c186a3e0e34dd15693a
mynerva0x13 <koutayuzuki565@gmail.com>
2026-06-27 15:46:28 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+     public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY
+    },
+},
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
