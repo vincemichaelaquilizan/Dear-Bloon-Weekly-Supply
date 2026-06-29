0b8ee6b0c330fefcf378d4088614585bd3e1fb6d
mynerva0x13 <koutayuzuki565@gmail.com>
2026-06-29 20:13:19 +0800

    fixed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/supabase',
     '@pinia/nuxt',
   ],
-  // supabase: {
-  //   redirect: false,
-  //   clientOptions: {
-  //     auth: {
-  //       persistSession: true,       // store session in localStorage
-  //       autoRefreshToken: true,     // keep the session alive automatically
-  //       detectSessionInUrl: true,   // needed for magic links / OAuth callbacks
-  //       storageKey: 'sb-auth',      // explicit key for reliable persistence
-  //     },
-  //   },
-  // },
+  supabase: {
+    redirect: false,
+    clientOptions: {
+      auth: {
+        persistSession: true,       // store session in localStorage
+        autoRefreshToken: true,     // keep the session alive automatically
+        detectSessionInUrl: true,   // needed for magic links / OAuth callbacks
+        storageKey: 'sb-auth',      // explicit key for reliable persistence
+      },
+    },
+  },
   css: ['~/assets/css/main.css'],
   typescript: {
     strict: true,
